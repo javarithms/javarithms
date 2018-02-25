e7e47f5b47eab6e212169415d8562f02cc5264cf
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2018-02-25 12:16:05 +0800

    Binary search upper and lower bound implementations
    Signed-off-by: Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>

--- src/main/java/com/manparvesh/javarithms/java/algorithms/searching/BinarySearch.java
+++ src/main/java/com/manparvesh/javarithms/java/algorithms/searching/BinarySearch.java
@@ -1,5 +1,7 @@
 package com.manparvesh.javarithms.java.algorithms.searching;
 
+import com.manparvesh.javarithms.java.datastructures.misc.Pair;
+
 public class BinarySearch {
     public int binarySearchIterative(int elementToSearch, int[] arrayToSearchIn) {
         return binarySearchIterativeUtil(elementToSearch, arrayToSearchIn, 0, arrayToSearchIn.length - 1);
@@ -39,5 +41,77 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+
+    /**
+     * returns the index of the first element in the range
+     * [first,last) which does not compare less than val
+     *
+     * @param val
+     * @param ar
+     * @return
+     */
+    public int lowerBound(int val, int ar[]) {
+        int left = 0, right = ar.length - 1;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            if (ar[mid] == val) {
+                // found the element, now move to its leftmost occurrence
+                int it = mid;
+                while (ar[mid] == ar[it]) {
+                    it--;
+                }
+                return it;
+            }
+            if (ar[mid] > val) {
+                // move left
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * returns the index of the first element in the range
+     * [first,last) which is strictly grater than val
+     *
+     * @param val
+     * @param ar
+     * @return
+     */
+    public int upperBound(int val, int ar[]) {
+        int left = 0, right = ar.length - 1;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            if (ar[mid] == val) {
+                // found the element, now move to its rightmost occurrence
+                int it = mid;
+                while (ar[mid] == ar[it]) {
+                    it++;
+                }
+                return it + 1;
+            }
+            if (ar[mid] > val) {
+                // move left
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * returns a pair of lower and upper bound for an array
+     * relative to a given integer x
+     *
+     * @param element integer
+     * @param array   array
+     * @return a pair of lower and upper bound for an array
+     */
+    public Pair<Integer, Integer> equalRange(int element, int array[]) {
+        return new Pair<>(lowerBound(element, array), upperBound(element, array));
     }
 }
---
+++ src/main/java/com/manparvesh/javarithms/java/datastructures/misc/Pair.java
@@ -0,0 +1,11 @@
+package com.manparvesh.javarithms.java.datastructures.misc;
+
+public class Pair<K, E> {
+    public K first;
+    public E second;
+
+    public Pair(K first, E second) {
+        this.first = first;
+        this.second = second;
+    }
+}
